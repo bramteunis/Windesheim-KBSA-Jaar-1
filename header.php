a195af68cc1af9897faf878700492259141d62cd
RickBeniers <Beniersrick@gmail.com>
2021-11-03 10:37:27 +0100

    new commit

--- header.php
+++ header.php
@@ -47,14 +47,14 @@ $databaseConnection = connectToDatabase();
                 </li>
             </ul>
         </div>
-<!-- code voor US3: zoeken -->
+<!-- code voor US3: zoeken. -->
         <ul id="ul-class-navigation">
             <li>
                 <a href="browse.php" class="HrefDecoration"><i class="fas fa-search search"></i> Zoeken</a>
             </li>
         </ul>
 
-<!-- einde code voor US3 zoeken -->
+<!-- einde code voor US3 zoeken. -->
     </div>
     <div class="row" id="Content">
         <div class="col-12">
