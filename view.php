0a9d975095e2b7da556207f5de039a05c30553e1
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-23 12:28:14 +0100

    Update view.php

--- view.php
+++ view.php
@@ -116,7 +116,7 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
                             if (isset($_POST["submit"])) {              // zelfafhandelend formulier
                                 $stockItemID = $_POST["stockItemID"];
                                 addProductToCart($stockItemID);         // maak gebruik van geïmporteerde functie uit cartfuncties.php
-                                print("Product toegevoegd aan <a href='cart.php'> ".$stockItemID."</a>");
+                                print("<h1>Product toegevoegd aan <a href='cart.php'> ".$stockItemID."</a></h1>");
                             }
                         ?>
                     </div>
