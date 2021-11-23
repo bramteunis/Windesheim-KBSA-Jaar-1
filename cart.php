7e5c49f34aeacc1635c85f636571f1bd1cc193f0
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-23 13:07:25 +0100

    Informatie opvragen over producten in winkelmand

--- cart.php
+++ cart.php
@@ -11,8 +11,20 @@ include "cartfuncties.php";
 <h1>Inhoud Winkelwagen</h1>
 
 <?php
+include "cartfuncties.php";
+include __DIR__ . "/header.php";
+
 $cart = getCart();
+foreach($cart as $artikelnummer => $aantalartikel){
+    $StockItem = getStockItem($artikelnummer, $databaseConnection);
+    $StockItemImage = getStockItemImage($artikelnummer, $databaseConnection);
+    print ($StockItem['StockItemName']);
+}
 print_r($cart);
+
+
+
+
 //gegevens per artikelen in $cart (naam, prijs, etc.) uit database halen
 //totaal prijs berekenen
 //mooi weergeven in html
