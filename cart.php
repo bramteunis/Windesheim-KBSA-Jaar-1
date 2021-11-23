eb59c99af3892988efafa8e085379f973a5fde29
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-23 14:21:13 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -19,7 +19,7 @@ foreach($cart as $artikelnummer => $aantalartikel){
     $StockItem = getStockItem($artikelnummer, $databaseConnection);
     $StockItemImage = getStockItemImage($artikelnummer, $databaseConnection);
     print ("<h1 style='color:black;'>".$StockItem['StockItemName']."</h1>");
-    print ("<img src=("."public/stockitemimg/".strtolower($StockItemImage[0]['ImagePath']).")>");
+    print ("<img src="."public/stockitemimg/".str_replace(" ", "%20",strtolower($StockItemImage[0]['ImagePath'])).">");
 }
 print_r($cart);
 
