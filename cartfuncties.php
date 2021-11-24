5b1cdc9ba320b169b06b370fb9bdcaf31477b6b1
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-24 13:09:33 +0100

    Update cartfuncties.php

--- cartfuncties.php
+++ cartfuncties.php
@@ -34,7 +34,7 @@ function removeProductFromCart($stockItemID){
     }else{
         $cart[$stockItemID] = 0;                    //zo nee: key toevoegen en aantal op 1 zetten.
     }
-    //debug_to_console($cart);
+    debug_to_console("Aantal van artikel ".$stockItemID.": ".$cart[$stockItemID]);
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
 function debug_to_console($data) {
