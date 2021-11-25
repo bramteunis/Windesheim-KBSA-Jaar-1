41dfd05b0333be08f66b092902b140b0eb320d19
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-25 09:34:49 +0100

    Create phpcodetester.php

---
+++ phpcodetester.php
@@ -0,0 +1,44 @@
+<?php
+include __DIR__ . "/header.php";
+$Query = "
+           SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, TaxRate, RecommendedRetailPrice,
+           ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) as SellPrice,
+           QuantityOnHand,
+           (SELECT ImagePath FROM stockitemimages WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+           (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+           FROM stockitems SI
+           JOIN stockitemholdings SIH USING(stockitemid)
+           JOIN stockitemstockgroups USING(StockItemID)
+           JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
+           WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
+           GROUP BY StockItemID
+           ORDER BY " . $Sort . "
+           LIMIT ? OFFSET ?";
+    
+    debug_to_console("Tweede keer queryBuildResult: ".$queryBuildResult);
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_bind_param($Statement, "iii", $CategoryID, $ProductsOnPage, $Offset);
+    mysqli_stmt_execute($Statement);
+    $ReturnableResult = mysqli_stmt_get_result($Statement);
+    $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
+    debug_to_console("Eerste keer ReturnableResult: ".print_r($ReturnableResult));
+    $Query = "
+                SELECT count(*)
+                FROM stockitems SI
+                WHERE " . $queryBuildResult . " ? IN (SELECT SS.StockGroupID from stockitemstockgroups SS WHERE SS.StockItemID = SI.StockItemID)";
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_bind_param($Statement, "i", $CategoryID);
+    mysqli_stmt_execute($Statement);
+    $Result = mysqli_stmt_get_result($Statement);
+    $Result = mysqli_fetch_all($Result, MYSQLI_ASSOC);
+}
+
+if (isset($ReturnableResult) && count($ReturnableResult) > 0) {
+  foreach ($ReturnableResult as $row) {
+	    debug_to_console("Eerste keer row: ".$row["RecommendedRetailPrice"]);
+    
+function berekenVerkoopPrijs($adviesPrijs, $btw) {
+		return $btw * $adviesPrijs / 100 + $adviesPrijs;
+}
+    
+?>
