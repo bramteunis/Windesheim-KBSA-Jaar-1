f13bd4f295fab62151255c27fcb34d9ee15476f4
RickBeniers <Beniersrick@gmail.com>
2021-11-25 10:27:59 +0100

    added divjes and style to cart.php, 25-11-2021

--- cart.php
+++ cart.php
@@ -1,5 +1,6 @@
 <?php
 include "cartfuncties.php";
+include __DIR__ . "/header.php";
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -9,33 +10,34 @@ include "cartfuncties.php";
     <link rel="stylesheet" href="public/css/style.css" type="text/css">
 </head>
 <body>
-<h1>Inhoud Winkelwagen</h1>
-
+    <div id="cartBackground">
+        <!-- <p><a href='view.php?id=0'>Naar artikelpagina van artikel 0</a></p> -->
+        <div id="titleCart">
+            <h1 id="titleText">Winkelmand</h1>
+            <button id="verderWinkelenKnop">Verder winkelen</button>
+            <button id="AfrekenenKnop">Afrekenen</button>
+        </div>
 <?php
-
-include __DIR__ . "/header.php";
-
 $cart = getCart();
 foreach($cart as $artikelnummer => $aantalartikel){
     $StockItem = getStockItem($artikelnummer, $databaseConnection);
     $StockItemImage = getStockItemImage($artikelnummer, $databaseConnection);
+
     print ("<h1 style='color:black;'>".$StockItem['StockItemName']."</h1>");
     print ("<img src="."public/stockitemimg/".str_replace(" ", "%20",strtolower($StockItemImage[0]['ImagePath'])).">");
-
-    
     print('<form method="post">
     <input type="number" name="stockItemID" value="print($artikelnummer)" hidden>
     <input class="ToevoegenWinkelmandbutton ToevoegenWinkelmandbutton1" type="submit" name="submit" value="Verwijderen uit winkelmandje">
     </form>');
+
     if (isset($_POST["submit"])) {              // zelfafhandelend formulier
         $stockItemID = $artikelnummer;
         removeProductFromCart($stockItemID);         // maak gebruik van geïmporteerde functie uit cartfuncties.php
-        
     }
 }
 print_r($cart);
 
 ?>
-<p><a href='view.php?id=0'>Naar artikelpagina van artikel 0</a></p>
+    </div>
 </body>
 </html>
