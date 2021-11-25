14ae7a3a7274b86edf7f70ca1d0ef200c4edf969
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-25 13:39:04 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -16,7 +16,43 @@ include "cartfuncties.php";
 <?php
 
 include __DIR__ . "/header.php";
- 
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+function berekenVerkoopPrijs($adviesPrijs, $btw) {
+		return $btw * $adviesPrijs / 100 + $adviesPrijs;
+}
+$Query = "
+           SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, TaxRate, RecommendedRetailPrice,
+           ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) as SellPrice,
+           QuantityOnHand,
+           (SELECT ImagePath FROM stockitemimages WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+           (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+           FROM stockitems SI
+           JOIN stockitemholdings SIH USING(stockitemid)
+           JOIN stockitemstockgroups USING(StockItemID)
+           JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
+           WHERE 'iii' NOT IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
+           GROUP BY StockItemID";
+    
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_execute($Statement);
+    $ReturnableResult = mysqli_stmt_get_result($Statement);
+    $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
+
+
+if (isset($ReturnableResult) && count($ReturnableResult) > 0) {
+   foreach ($ReturnableResult as $row) {
+	    debug_to_console("Prijs van: ".$row["StockItemID"]."is: "."€".sprintf(" %0.2f", berekenVerkoopPrijs($row["RecommendedRetailPrice"], $row["TaxRate"])));
+   }
+}
+    
+    
 $cart = getCart();
 foreach($cart as $artikelnummer => $aantalartikel){
     if($aantalartikel > 0){
