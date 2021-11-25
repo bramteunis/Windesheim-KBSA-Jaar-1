92f515f488852db3f588f14b1acf8dd81717c16c
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-25 13:02:48 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -30,8 +30,36 @@ foreach($cart as $artikelnummer => $aantalartikel){
         
         
         //print ("<h1 class='StockItemPriceText'>".'€'.sprintf('%0.2f', berekenVerkoopPrijs($row['RecommendedRetailPrice'], $row['TaxRate']))."</h1>");
-           
-        print('<form method="post">');
+        function berekenVerkoopPrijs($adviesPrijs, $btw) {return $btw * $adviesPrijs / 100 + $adviesPrijs;}
+        $Query = "
+                   SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, TaxRate, RecommendedRetailPrice,
+                   ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) as SellPrice,
+                   QuantityOnHand,
+                   (SELECT ImagePath FROM stockitemimages WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+                   (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+                   FROM stockitems SI
+                   JOIN stockitemholdings SIH USING(stockitemid)
+                   JOIN stockitemstockgroups USING(StockItemID)
+                   JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
+                   WHERE 'iii' NOT IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
+                   GROUP BY StockItemID";
+
+            $Statement = mysqli_prepare($databaseConnection, $Query);
+            mysqli_stmt_execute($Statement);
+            $ReturnableResult = mysqli_stmt_get_result($Statement);
+            $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
+
+
+        if (isset($ReturnableResult) && count($ReturnableResult) > 0) {
+            foreach ($ReturnableResult as $row) {
+                if($row["StockItemID"] == $aantalartikel){
+                //debug_to_console("Prijs van: ".$row["StockItemID"]."is: "."€".sprintf(" %0.2f", berekenVerkoopPrijs($row["RecommendedRetailPrice"], $row["TaxRate"])));
+                    print ("<h1 class='StockItemPriceText'>".'€'.sprintf('%0.2f', berekenVerkoopPrijs($row['RecommendedRetailPrice'], $row['TaxRate']))."</h1>");
+                }
+           }
+        }
+
+        print('<form method="post">');  
         print('<input type="number" name="stockItemID" value="print($artikelnummer)" hidden>');
         print('<input class="ToevoegenWinkelmandbutton ToevoegenWinkelmandbutton1" type="submit" name="submit" value="Verwijderen uit winkelmandje">');
         print('</form>');
