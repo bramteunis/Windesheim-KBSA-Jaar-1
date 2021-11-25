81b5e6903120abfc988b0cd9cad9c3fee931de6a
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-25 10:40:57 +0100

    Update phpcodetester.php

--- phpcodetester.php
+++ phpcodetester.php
@@ -189,7 +189,13 @@ $amount = $Result[0];
 if (isset($amount)) {
     $AmountOfPages = ceil($amount["count(*)"] / $ProductsOnPage);
 }
-
+    function getVoorraadTekst($actueleVoorraad) {
+        if ($actueleVoorraad > 1000) {
+            return "Ruime voorraad beschikbaar.";
+        } else {
+            return "Voorraad: $actueleVoorraad";
+        }
+    }
     function berekenVerkoopPrijs($adviesPrijs, $btw) {
 		return $btw * $adviesPrijs / 100 + $adviesPrijs;
     }
