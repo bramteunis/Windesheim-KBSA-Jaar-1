93e5c2ce250783ead1189860ec56d89bbb60b11a
RickBeniers <Beniersrick@gmail.com>
2021-11-26 15:21:25 +0100

    flex-container toegevoegd zodat alle producten in het winkelmandje goed worden weergegeven in een colum, 26-11-2021

--- cart.php
+++ cart.php
@@ -11,7 +11,6 @@ include __DIR__ . "/header.php";
 </head>
 <body>
     <div id="cartBackground">
-        <!-- <p><a href='view.php?id=0'>Naar artikelpagina van artikel 0</a></p> -->
         <div id="titleCart">
             <h1 id="titleText">Winkelmand</h1>
             <button id="verderWinkelenKnop">Verder winkelen</button>
@@ -22,9 +21,9 @@ $cart = getCart();
 foreach($cart as $artikelnummer => $aantalartikel){
     $StockItem = getStockItem($artikelnummer, $databaseConnection);
     $StockItemImage = getStockItemImage($artikelnummer, $databaseConnection);
-    print("<div class='productCart'style='border:2px solid black;margin-top:115px;width:1848px;height: 125px;'>");
-    print("<div class='flex-container' style='float:left;width:1500px;height:125px;display:flex'>");
-    print ("<img style='float: left;width:125px;height:125px;'src="."public/stockitemimg/".str_replace(" ", "%20",strtolower($StockItemImage[0]['ImagePath'])).">");
+    print("<div style='border:2px solid black;margin-top:10px;width:1848px;height:125px;'>");
+    print("<div class='flex-container' style='float:left;width:1500px;height:125px;display:flex;'>");
+    print ("<img style='float:left;width:110px;height:110px;margin-top:5px;margin-left:5px;'src="."public/stockitemimg/".str_replace(" ", "%20",strtolower($StockItemImage[0]['ImagePath'])).">");
     print ("<h5 style='color:black; margin-left: 50px;margin-top:15px;width:500px;height:50px'>".$StockItem['StockItemName']."</h5>");
     print("<h5 style='color: black; margin-left: 50px;margin-top:15px;float:right;'>voorraad beschikbaarheid</h5>");
     print("</div>");
