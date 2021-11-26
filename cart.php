392a8e1f5f105fdc3c1dbabbb1efc71425068f5c
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-26 14:21:51 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
 <?php
 
-include __DIR__ . "cartfuncties.php";
-include __DIR__ . "header.php";
+include __DIR__ ."cartfuncties.php";
+include __DIR__ ."header.php";
 
 
 function debug_to_console($data) {
