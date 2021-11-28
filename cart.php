ffb2a741b828f1688ff7994095c1c6338c90feb0
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-28 17:06:56 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -64,7 +64,7 @@ foreach($cart as $artikelnummer => $aantalartikel)
                 print("<h6 style='color:black;width:140px;height:30px;float:right;margin-top:10px;margin-right:10px;align-content:center;'> €". $cart[$artikelnummer] * sprintf('%0.2f', berekenVerkoopPrijs($row['RecommendedRetailPrice'], $row['TaxRate']))."</h6>");
                 //print("<h1 style='color:black;'>".$row['MarketingComments']."</h1>");
                 if(preg_replace('/[^0-9]/./', '', $row["SendCosts"]) > $hoogsteverzending){
-                      $hoogsteverzending = preg_replace('/[^0-9]/./', '', $row["SendCosts"]);
+                      $hoogsteverzending = preg_replace('/[^0-9]/[.]/', '', $row["SendCosts"]);
                            
                 }
             }
