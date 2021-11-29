c018e58b432bd59381d5a8419c8222ff990ef09a
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-29 22:41:05 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -98,11 +98,11 @@ foreach($cart as $artikelnummer => $aantalartikel)
                    if($_POST["format"] == "") {
                        //print("selected aantal van ".$artikelnummer." is => " . $country[$artikelnummer]);
                        updateProductFromCart($artikelnummer,$country[$artikelnummer]);
-                       echo("<meta http-equiv='refresh' content='1'>"); 
+                       //echo("<meta http-equiv='refresh' content='1'>"); 
                    }else{
                        //print("selected aantal ".$artikelnummer." is => " . $country[$artikelnummer]=$_POST["format"]);
                        updateProductFromCart($artikelnummer,$country[$artikelnummer]);
-                       echo("<meta http-equiv='refresh' content='1'>"); 
+                       //echo("<meta http-equiv='refresh' content='1'>"); 
                    }
            }
     if (isset($ReturnableResult) && count($ReturnableResult) > 0) {
