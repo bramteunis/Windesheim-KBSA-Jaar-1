9077f3634079701bc1fae52d68c723c96964d489
RickBeniers <Beniersrick@gmail.com>
2021-11-29 15:44:42 +0100

    cart.php basis responsive gemaakt, 29-11-2021

--- cart.php
+++ cart.php
@@ -51,18 +51,18 @@ foreach($cart as $artikelnummer => $aantalartikel)
 {
     $StockItem = getStockItem($artikelnummer, $databaseConnection);
     $StockItemImage = getStockItemImage($artikelnummer, $databaseConnection);
-    print("<div style='border:2px solid black;margin-top:10px;width:1848px;height:125px;'>");
-    print("<div class='flex-container' style='float:left;width:1500px;height:125px;display:flex;'>");
-    print ("<img style='float:left;width:110px;height:110px;margin-top:5px;margin-left:5px;'src="."public/stockitemimg/".str_replace(" ", "%20",strtolower($StockItemImage[0]['ImagePath'])).">");
-    print ("<h5 style='color:black; margin-left: 50px;margin-top:15px;width:500px;height:50px'>".$StockItem['StockItemName']."</h5>");
-    print("<h5 style='color: black; margin-left: 50px;margin-top:15px;float:right;'>voorraad beschikbaarheid</h5>");
+    print("<div class='productCard'>");
+    print("<div class='flex-container leftProductCard'>");
+    print ("<img class='productImage'src="."public/stockitemimg/".str_replace(" ", "%20",strtolower($StockItemImage[0]['ImagePath'])).">");
+    print ("<h5 class='productName'>".$StockItem['StockItemName']."</h5>");
+    print("<h5 class='productStockAmount'>voorraad beschikbaarheid</h5>");
     print("</div>");
-    print("<div style='float:right;width:344px;height:125px;'>");
+    print("<div class='rightProductCard'>");
     print('<form method="post">
-    <div style="width:344px;height:62px;">
+    <div class="upperRightProductCard">
     <input type="number" name="stockItemID" value="print($artikelnummer)" hidden>
-    <input type="number" value="1" id="rangeInputForm">
-    <h6 style="color:black;width:140px;height:30px;float:right;margin-top:10px;margin-right:10px;align-content:center;">prijs_placeholder</h6>
+    <input type="number" value="1" class="rangeInputForm">
+    <h6 class="prijsWeergave">prijs_placeholder</h6>
     </div>
     <input class="ToevoegenWinkelmandbutton ToevoegenWinkelmandbutton1" type="submit" name="submit" value="Verwijderen">
     </form>');
