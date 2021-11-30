db2c91e6076aa78d238d36e0b36b2e0bb6eb585c
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-11-30 22:24:07 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -49,7 +49,7 @@ $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
 
                             //$Statement2 = mysqli_prepare($databaseConnection, $Query2);
                             //mysqli_stmt_execute($Statement2);     
-                            debug_to_console("Nieuwevooraad van artikel: ". $artikelnummer." is: ".$aantalartikel);
+                            debug_to_console("Nieuwevooraad van artikel: ". $artikelnummer." is: ".$nieuwevoorraad);
                             }
                  }
             ?>
