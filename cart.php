fca989fdcc63ee7ed11015098f68e445d563a39f
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-01 08:43:52 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -45,7 +45,7 @@ $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
                             $StockItem = getStockItem($artikelnummer, $databaseConnection);
                             $currentvoorraad = $StockItem['QuantityOnHand'];
                             
-                            $nieuwevoorraad = $currentvoorraad - 1;
+                            $nieuwevoorraad = 1 - $currentvoorraad ;
                             //$Query2 = "
                             //     UPDATE stockitemholdings set qualityonhand=".$nieuwevoorraad." WHERE stockitemid=".$artikelnummer;
 
