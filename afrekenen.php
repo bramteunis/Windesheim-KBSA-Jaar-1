49e8301dc02d150f5d6be915eed8ac985d752758
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-02 11:21:35 +0100

    Update afrekenen.php

--- afrekenen.php
+++ afrekenen.php
@@ -35,7 +35,40 @@
             <input class="no-outline" type="text" name="achternaam" required>
         </div>
             <span class="underline"></span>
+        <?php
+            function testinconsole(){
+                debug_to_console("Test is geslaagd");
+            }
+            print('<form method="POST" action="" onsubmit="testinconsole()"><input type="submit" name="afrekenensubmit2" value="Afrekenen"></form>');
+            if(isset($_POST["afrekenensubmit2"])){  
+                $voornaam = $_POST["voornaam"];
+                $achternaam = $_POST["achternaam"];
+                $postcode = $_POST["postcode"];
+                $straatnaam = $_POST["straatnaam"];
+                $email = $_POST["email"];
+                $telefoonnummer = $_POST["telefoonnummer"];
+                $tussenvoegsel = $_POST["tussenvoegsel"];
+                $huisnummer = $_POST["huisnummer"];
+                $plaats = $_POST["plaats"];
+                $date = date('Y-m-d H:i:s');
+                
+                $Query2 = "INSERT INTO nerdygadgets.customers (CustomerID, CustomerName, BillToCustomerID, CustomerCategoryID, BuyingGroupID, PrimaryContactPersonID, 
+                           AlternateContactPersonID, DeliveryMethodID, DeliveryCityID, PostalCityID, CreditLimit, AccountOpenedDate, StandardDiscountPercentage, 
+                           IsStatementSent, IsOnCreditHold, PaymentDays, PhoneNumber, FaxNumber, DeliveryRun, RunPosition, WebsiteURL, DeliveryAddressLine1, 
+                           DeliveryAddressLine2, DeliveryPostalCode, DeliveryLocation, PostalAddressLine1, PostalAddressLine2, PostalPostalCode, LastEditedBy, 
+                           ValidFrom, ValidTo) VALUES ('1062', ".$voornaam.' '.$tussenvoegsel.' '.$achternaam.", '1062', '0', '0', '0', '0', '3', '".$postcode."', '".$postcode."', '100000', ".$date.", '0', '0', 
+                           '0', '7', ".$telefoonnummer.", ".$telefoonnummer.", 'null', 'null', 'null', ".$huisnummer.", ".$straatnaam.", ".$postcode.", 'null', 'null', 'null', 'null', 
+                           0, ".$date.", '9999-12-31');";
+                
+                $Statement2 = mysqli_prepare($databaseConnection, $Query2);
+                mysqli_stmt_execute($Statement2);     
+            }
+        ?>
     </form>
     </div>
 </body>
 </html>
+<?php
+
+
+?>
