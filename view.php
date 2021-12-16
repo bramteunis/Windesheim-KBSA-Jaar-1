370bdf886f751cea40398864f48a7ae641454dcc
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-16 14:15:31 +0100

    Update view.php

--- view.php
+++ view.php
@@ -157,7 +157,7 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
                          formulier via POST en niet GET om te zorgen dat refresh van pagina niet het artikel onbedoeld toevoegt-->
                         <form method="post">
                             <input type="number" name="stockItemID" value="<?php print($stockItemID) ?>" hidden>
-                            <?php if($StockItem['QuantityOnHand'] != 0){ ?>
+                            <?php if($StockItem['QuantityOnHand'] != "Voorraad: 0"){ ?>
                             <input class="ToevoegenWinkelmandbutton ToevoegenWinkelmandbutton1" type="submit" name="submit" value="Toevoegen winkelmand">
                             <?php } ?>
                         </form>
