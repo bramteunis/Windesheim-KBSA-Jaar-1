6425adb87121232a6c0380199149d6e053c75018
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-16 21:56:18 +0100

    Update view.php

--- view.php
+++ view.php
@@ -5,8 +5,9 @@ include "cartfuncties.php";
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <title>Artikelpagina (geef ?id=.. mee)</title>
+    <title>Artikelpagina</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="3" >
 </head>
 <body>
 
