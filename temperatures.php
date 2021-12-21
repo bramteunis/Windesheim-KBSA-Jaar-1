6588530aef4b9418a4c858c19dec4e1ebc505752
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-21 20:34:02 +0100

    Update temperatures.php

--- temperatures.php
+++ temperatures.php
@@ -1,3 +1,9 @@
+<?php
+include __DIR__ . "/cartfuncties.php";
+include __DIR__ . "/header.php";
+
+
+?>
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -11,11 +17,34 @@
 <script type="text/javascript" src="//use.typekit.net/uvs8amk.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
+<?php
+  
+  $Query = "SELECT * FROM coldroomtemperatures ORDER BY ColdRoomTemperatureID DESC LIMIT 1";
+  $Statement2 = mysqli_prepare($databaseConnection2, $Query);
+  mysqli_stmt_execute($Statement2);
+
+  $ReturnableResult2 = mysqli_stmt_get_result($Statement2);
+  $ReturnableResult2 = mysqli_fetch_all($ReturnableResult2, MYSQLI_ASSOC);
+  foreach ($ReturnableResult2 as $row) {
+      print("Actuele Temperatuur: ".$row["Temperature"]);
+      $temp = $row["Temperature"];
+      $percentage1 = ((33.4 - $temp)/33.4)*100;
+      if($temp < 33.4){
+        print("<div><span>Progressive</span><b><i>$</i>74</b><small><i>$</i>354</small><strong><em class="icon icon-chevron-down"></em>".$percentage1."%</strong><a href="#">SELECT</a></div>");
+      }else{
+        print("<div><span>Progressive</span><b><i>$</i>74</b><small><i>$</i>354</small><strong><em class="icon icon-down icon-chevron-up"></em>".$percentage1."%</strong><a href="#">SELECT</a></div>");
+      }
+  }
+  
+?>
+<!--
 <div><span>Progressive</span><b><i>$</i>74</b><small><i>$</i>354</small><strong><em class="icon icon-chevron-down"></em>7%</strong><a href="#">SELECT</a></div>
 
 <div><span>The General</span><b><i>$</i>89</b><small><i>$</i>379</small><strong><em class="icon icon-chevron-down"></em>5%</strong><a href="#">SELECT</a></div>
 
 <div><span>Esurance</span><b><i>$</i>92</b><small><i>$</i>432</small><strong><em class="icon icon-down icon-chevron-up"></em>4%</strong><a href="#">SELECT</a></div>
+
+-->
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 </body>
