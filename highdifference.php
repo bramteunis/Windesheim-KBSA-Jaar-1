0072d6834599d410f9e59b4051090b3987107c5e
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-22 23:20:35 +0100

    Update highdifference.php

--- highdifference.php
+++ highdifference.php
@@ -25,8 +25,8 @@ $databaseConnection2 = connectToDatabase2();
 <a href='highdifference.php' style='float:none;background:#03625a;'>Hoogste afwijking</a>
 <a href='lowdifference.php' style='float:none;'>Kleinste afwijking</a>
 <br>
-<a href='positivehighdifference.php' style='float:none'>Positieve afwijking</a>
-<a href='negativelowdifference.php' style='float:none;'>Negatieve afwijking</a>
+<a href='positivehighdifference.php' style='float:none;margin-top:10px;'>Positieve afwijking</a>
+<a href='negativelowdifference.php' style='float:none;margin-top:10px;'>Negatieve afwijking</a>
 <?php
   
    
