4b3f20681b39465fbb46298e4edb06cca7faa0fa
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-22 23:51:42 +0100

    Update temperatures.php

--- temperatures.php
+++ temperatures.php
@@ -4,6 +4,9 @@ include __DIR__ . "/database.php";
 
 $databaseConnection = connectToDatabase();
 $databaseConnection2 = connectToDatabase2();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 ?>
 <!DOCTYPE html>
