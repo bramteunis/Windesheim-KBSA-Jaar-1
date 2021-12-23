089ded27fe5e2fbf8f6c2c7bfdfb7d7956b97d26
bramteunis <51489074+bramteunis@users.noreply.github.com>
2021-12-23 12:21:01 +0100

    Update temperatures.php

--- temperatures.php
+++ temperatures.php
@@ -13,7 +13,7 @@ error_reporting(E_ALL);
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Carrier listing</title>
+  <title>Temperaturen</title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.0/css/font-awesome.min.css'><link rel="stylesheet" href="./style.css">
 
 </head>
