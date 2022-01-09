8d02d1776213567d77c3d6cb3770072847127885
bramteunis <51489074+bramteunis@users.noreply.github.com>
2022-01-09 23:26:07 +0100

    Update account.php

--- account.php
+++ account.php
@@ -1,16 +1,57 @@
 <?php
-echo "<div style='margin-top: 20px; text-align: center'>";
-if(isset($_SESSION['EmailAddress'])) {
-    $Gebruiker_Gebruikersnaam = $_SESSION['PreferredName'];
-    $Gebruiker_Voornaam = $_SESSION['FullName'];
-    $Gebruiker_Achternaam = $_SESSION['Gebruiker_Achternaam'];
-    $Gebruiker_Email = $_SESSION['EmailAddress'];
 
-    echo "<br>";
-    echo $Gebruiker_Gebruikersnaam . "<br>";
-    echo $Gebruiker_Voornaam . " " . $Gebruiker_Achternaam . "<br>";
-    echo $Gebruiker_Email . "<br><br><br><div style='margin-top: 20px; text-align: center'><a class='uitloggen' href='uitloggen.php'>Uitloggen</a ></div>";} else {
-    include "login.php";
-}
-
+include __DIR__ . "/header.php";
+include __DIR__ . "/inloggen.php";
+$databaseConnection = connectToDatabase();
 ?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Account</title>
+</head>
+<?php
+    $query = "SELECT `:FullName`, `PreferredName`, `EmailAddress` FROM people";
+    $Statement = mysqli_prepare($databaseConnection, $query);
+    mysqli_stmt_execute($Statement);
+    $R = mysqli_stmt_get_result($Statement);
+    $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
+
+    //    if($R != "test"){
+    //        print($query);
+    //    }
+
+    // Opvragen van de informatie van de gebruiker
+    if(isset($_POST['FullName'])) {
+        if (isset($_POST['PreferredName'])) {
+            if (isset($_POST['psw'])) {
+                if (isset($_POST['EmailAddress'])) {
+                    if (isset($_POST['PhoneNumber'])) {
+                        echo '<table align="center" border="1px" style="width:600px; line-height:40px;">';
+                        echo '<tr>';
+                        echo '<th colspan="4" style="color: black"><h2>Gegevens</h2></th>';
+                        echo '</tr>';
+                        echo '<th style="color: black"> Naam </th>';
+                        echo '<th style="color: black"> Gebruik </th>';
+                        echo '<th style="color: black"> Email </th>';
+                        echo '</tr>';
+                        while($rows = $R)
+                        {
+                            ?>
+                            <tr> <td style="color: black"><?php echo $rows['FullName']; ?></td>
+                                <td style="color: black"><?php echo $rows['PreferredName']; ?></td>
+                                <td style="color: black"><?php echo $rows['EmailAddress']; ?></td>
+                            </tr>
+                            <?php
+                        }
+                        echo '</table>';
+                    }
+                }
+            }
+        }
+    }
+?>
+
+
+
